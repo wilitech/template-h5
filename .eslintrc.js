156f2ce2bde06ab266f40ec80b03d6e02dca1f36
wangrun <yeesunday@gmail.com>
2018-11-23 21:06:41 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: "module"
+  },
+  extends: [
+    'plugin:prettier/recommended'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
---
+++ src/pages/index/index.js
@@ -0,0 +1,4 @@
+import $ from 'jquery'
+import './index.scss'
+
+console.log(new Set([1, 1, 2, 3]))
